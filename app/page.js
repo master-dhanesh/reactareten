95914de6a1a5de8c9ff9fbaf1b573a6550eb225a
master-dhanesh <dhanesh1296@gmail.com>
2023-08-10 18:23:46 +0530

    Todo App Compeleted 01

--- app/page.js
+++ app/page.js
@@ -1,95 +1,149 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const page = () => {
+    const [title, settitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [status, setStatus] = useState("due");
+
+    const [tasks, setTasks] = useState([]);
+
+    const [activeTask, setActiveTask] = useState(null);
+
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+
+        // validation
+        if (title.length < 5 || description.length < 20) {
+            toast.error(
+                "Title and Description must be more than 5 and 20 characters respectively"
+            );
+
+            return;
+        }
+
+        const newtask = {
+            date: new Date().toLocaleDateString(),
+            title,
+            description,
+            status,
+        };
+
+        setTasks([...tasks, newtask]);
+
+        settitle("");
+        setDescription("");
+        setStatus("due");
+    };
+
+    const UpdateHandler = (index) => {
+        const { title, description, status } = tasks[index];
+        settitle(title);
+        setDescription(description);
+        setStatus(status);
+        setActiveTask(index);
+    };
+
+    const UpdateTask = (e) => {
+        e.preventDefault();
+        const copyTasks = [...tasks];
+        copyTasks[activeTask] = {
+            ...copyTasks[activeTask],
+            title,
+            description,
+            status,
+        };
+        setTasks(copyTasks);
+        setActiveTask(null);
+        settitle("");
+        setDescription("");
+        setStatus("due");
+    };
+
+    const DeleteHandler = (index) => {
+        setTasks(tasks.filter((t, i) => i !== index));
+    };
+
+    let tasklist = (
+        <h2 className="mt-5 w-100 text-center text-danger">Loading...</h2>
+    );
+    if (tasks.length > 0) {
+        tasklist = tasks.map((task, index) => {
+            return (
+                <div
+                    key={index}
+                    className="card  mb-3 me-3"
+                    style={{ width: "18rem" }}
+                >
+                    <div className="card-body">
+                        <h5 className="card-title">{task.title}</h5>
+                        <h6 className="card-subtitle mb-2 text-body-secondary">
+                            {task.status}
+                        </h6>
+                        <p className="card-text">{task.description}</p>
+
+                        <button
+                            onClick={() => UpdateHandler(index)}
+                            className="me-2 btn btn-sm   btn-dark"
+                        >
+                            Update Task
+                        </button>
+                        <button
+                            onClick={() => DeleteHandler(index)}
+                            className="btn btn-sm btn-dark"
+                        >
+                            Delete Task
+                        </button>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    return (
+        <div className="mt-5 container p-5">
+            <form className="w-50">
+                <h2>Create Your Tasks</h2>
+                <input
+                    onChange={(e) => settitle(e.target.value)}
+                    value={title}
+                    className="form-control mb-3"
+                    type="text"
+                    placeholder="Title"
+                />
+                <textarea
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
+                    className="form-control mb-3"
+                    placeholder="description here..."
+                ></textarea>
+                <select
+                    onChange={(e) => setStatus(e.target.value)}
+                    className="form-control mb-3"
+                    value={status}
+                >
+                    <option value="due">Due</option>
+                    <option value="pending">Pending</option>
+                    <option value="completed">Completed</option>
+                </select>
+
+                {activeTask === null ? (
+                    <button onClick={SubmitHandler} className="btn btn-primary">
+                        Create Task
+                    </button>
+                ) : (
+                    <button onClick={UpdateTask} className="btn btn-primary">
+                        Update Task
+                    </button>
+                )}
+
+                <hr />
+            </form>
+            <h2>Pending Tasks</h2>
+            <div className="d-flex flex-wrap">{tasklist}</div>
         </div>
-      </div>
+    );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default page;

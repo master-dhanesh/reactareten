1fe2353f1ac8954863dc4797b866c2c046f7f279
master-dhanesh <dhanesh1296@gmail.com>
2023-08-24 18:25:05 +0530

    UseEffect 001

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 "use client";
+import Show from "@/components/Show";
 import axios from "@/utils/axios";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
     const [posts, setPosts] = useState([]);
+
+    const [show, setShow] = useState(false);
+
     const getPosts = async () => {
         try {
             const { data } = await axios.get("/posts?_limit=10");
@@ -14,13 +18,27 @@ const page = () => {
         }
     };
 
+    useEffect(() => {
+        console.log("Api called!");
+        getPosts();
+    }, []);
+
     return (
         <div className="container mt-5">
+            <h3>Click to Show Data</h3>
+            <button onClick={() => setShow(!show)} className="btn btn-primary">
+                {!show ? "Show" : "Hide"}
+            </button>
+
+            {show ? <Show /> : ""}
+
+            <hr />
+
             <h1>Axios Example</h1>
-            <button onClick={getPosts} className="btn btn-success">
+            {/* <button onClick={getPosts} className="btn btn-success">
                 Posts
             </button>
-            <hr />
+            <hr /> */}
 
             {posts.length !== 0
                 ? posts.map((p) => (
--- utils/axios.js
+++ utils/axios.js
@@ -1,12 +1,7 @@
-import axios from "axios";
+const { default: axios } = require("axios");
 
 const instance = axios.create({
     baseURL: "https://jsonplaceholder.typicode.com/",
-    // withCredentials: true
 });
 
 export default instance;
-
-// export const wait = (ms) => {
-//     return new Promise((res) => setTimeout(res, ms));
-// };
